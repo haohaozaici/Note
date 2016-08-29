d1348eab24adf8ea0d31a8ada89da37625b6dafc
haohaozaici <358297604@qq.com>
2016-08-29 21:19:38 +0800

    github地址点击跳转网页

--- app/src/main/java/com/example/hao/Note/AboutMe.java
+++ app/src/main/java/com/example/hao/Note/AboutMe.java
@@ -1,17 +1,37 @@
 package com.example.hao.Note;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
 /**
  * Created by hao on 2016-08-21.
  */
 public class AboutMe extends AppCompatActivity {
 
+    private LinearLayout github;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_me);
 
+        github = (LinearLayout) findViewById(R.id.github);
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openWebsite(getString(R.string.GitHub));
+            }
+        });
+
+    }
+
+    public void openWebsite(String url) {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(getResources().getColor(R.color.colorAccent));
+        builder.build().launchUrl(this, Uri.parse(url));
     }
 }
