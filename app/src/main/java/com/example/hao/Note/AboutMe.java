558680b55e76a085ec9115cc40f29d426eb541bb
haohaozaici <358297604@qq.com>
2016-08-30 00:07:03 +0800

    Add Alipay elegant

--- app/src/main/java/com/example/hao/Note/AboutMe.java
+++ app/src/main/java/com/example/hao/Note/AboutMe.java
@@ -7,12 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import moe.feng.alipay.zerosdk.AlipayZeroSdk;
+
 /**
  * Created by hao on 2016-08-21.
  */
 public class AboutMe extends AppCompatActivity {
 
     private LinearLayout github;
+    private LinearLayout Alipay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,17 @@ public class AboutMe extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 openWebsite(getString(R.string.GitHub));
+            }
+        });
+
+        Alipay = (LinearLayout) findViewById(R.id.Alipay);
+        Alipay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Boolean b = AlipayZeroSdk.hasInstalledAlipayClient(getApplicationContext());
+                if (b) {
+                    AlipayZeroSdk.startAlipayClient(AboutMe.this, getString(R.string.Alipay));
+                }
             }
         });
 
